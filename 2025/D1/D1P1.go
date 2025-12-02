20957f8f3edfc2fba544d44e6b762711f173f31a
ServerCrashed <aks3045@proton.me>
2025-12-02 11:15:09 +0530

    Add a second method for file access in `D1P1.go`

--- 2025/D1/D1P1.go
+++ 2025/D1/D1P1.go
@@ -1,16 +1,18 @@
-package d1
+package main
 
 import (
-	"bufio"
+	// "bufio"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 const (
 	// Starting position of the dial
 	INITIAL_POSITION = 50
+	INPUT_PATH       = "/home/akshat/repos/AoC/2025/D1/input.txt"
 )
 
 var (
@@ -55,16 +57,35 @@ func main() {
 	d := dial{INITIAL_POSITION}
 
 	//open file to read
-	f, err := os.Open("/home/akshat/repos/AoC/2025/D1/input.txt")
+	f, err := os.Open(INPUT_PATH)
 	if err != nil {
 		log.Fatalf("Error encountered while opening file: %v", err)
 	}
 	defer f.Close()
 
 	//for each line, do a rotate, and increment counter if the condition is satisfied
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := scanner.Text()
+
+	// Method 1
+	// scanner := bufio.NewScanner(f)
+	// for scanner.Scan() {
+	// 	line := scanner.Text()
+	// 	whetherZero := d.rotate(line)
+	// 	if whetherZero {
+	// 		counter += 1
+	// 	}
+	// }
+
+	// Method 2
+	data, err := os.ReadFile(INPUT_PATH)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
 		whetherZero := d.rotate(line)
 		if whetherZero {
 			counter += 1
